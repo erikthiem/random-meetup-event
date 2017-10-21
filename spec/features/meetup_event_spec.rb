f21498779edd05e724c0bb49e5dad8342d03ec8d
Erik Thiem <erikthiem@gmail.com>
2017-10-21 14:06:41 -0400

    Wrote specs and implementation for try again button

--- spec/features/meetup_event_spec.rb
+++ spec/features/meetup_event_spec.rb
@@ -8,6 +8,23 @@ RSpec.feature 'Display meetup event', type: :feature do
     stub_request(:get, %r{api.meetup.com/2/open_events}).with(headers: {'Accept'=>'*/*', 'Accept-Charset'=>'UTF-8', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).to_return(status: status, body: body , headers: {})
   end
 
+  context 'when there is more than one nearby event' do
+
+    let(:body) { {'results': [{name: event_name, event_url: event_url}, {name: event2_name, event_url: event2_url}]}.to_json }
+    let(:event_name) { 'Fun-sounding Thing' }
+    let(:event_url) { 'http://meetup/funthing' }
+    let(:event2_name) { 'something else' }
+    let(:event2_url) { 'http://meetup/otherthing' }
+
+    specify do
+      visit '/'
+      expect(find('#event-link').text).to be_in([event_name, event2_name])
+      expect(find('#event-link')[:href]).to be_in([event_url, event2_url])
+      click_link 'Anything else?'
+      expect(find('#event-link').text).to be_in([event_name, event2_name])
+      expect(find('#event-link')[:href]).to be_in([event_url, event2_url])
+    end
+  end
 
   context 'when there is at least one nearby event' do
 
