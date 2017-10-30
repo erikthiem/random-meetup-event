0f741414e831ccfa95b337c325ea1d7a8eaa64d7
Erik Thiem <erikthiem@gmail.com>
2017-10-30 00:15:22 -0400

    moved some logic from view to controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,9 @@
 class EventsController < ApplicationController
   def show
     @event = MeetupGetter.event
+    return if not @event
+    @group = @event['group']['name']
+    @start_time = Time.at(@event['time']/1000).strftime("%D %I:%M %p")
+    @end_time = Time.at(@event['time']/1000 + @event['duration']/1000).strftime("%D %I:%M %p")
   end
 end
